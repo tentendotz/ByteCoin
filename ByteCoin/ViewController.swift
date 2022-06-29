2ef505c2856b65eb8d89e37972ed51586665939c
ttt-net <tenten102matsuyama@gmail.com>
2022-06-30 02:42:51 +0900

    Add connection of UI to ViewController

--- ByteCoin/ViewController.swift
+++ ByteCoin/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
