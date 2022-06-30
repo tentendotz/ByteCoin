7ce9547519e8b351e30296d55aa69e648ccaf299
ttt-net <tenten102matsuyama@gmail.com>
2022-07-01 01:01:24 +0900

    Implement MVC and add baseURL

---
+++ ByteCoin/Models/CoinManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by tetsuta matsuyama on 2022/07/01.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+}
