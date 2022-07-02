9a583821ac7fb2d61d5b65864ac625994ed53557
ttt-net <tenten102matsuyama@gmail.com>
2022-07-02 16:41:10 +0900

    Add feature for API call

--- ByteCoin/Models/CoinManager.swift
+++ ByteCoin/Models/CoinManager.swift
@@ -15,9 +15,25 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
-        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-        print(urlString)
         
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
+            task.resume()
+        }
     }
     
+    
 }
+
