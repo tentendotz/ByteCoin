2e129e637f45cea678d3467d2510a2304308f2ca
tentendotz <tenten102matsuyama@gmail.com>
2023-02-07 22:09:15 +0900

    Update README.md

--- ByteCoin/Models/CoinManager.swift
+++ ByteCoin/Models/CoinManager.swift
@@ -17,7 +17,7 @@ struct CoinManager {
     var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "YOUR_API_KEY_HERE" // Need your API key. (https://www.coinapi.io)
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ByteCoin
 
 This App is the base of what I learned from Dr. Angela Yu @ The App Brewery.
+
+
+## Getting Started
+
+1. You need to sign up on the CoinAPI website to get your API key.  
+[https://www.coinapi.io/](https://www.coinapi.io/)
+
+1. Replace the apiKey property with yours in CoinManager.swift
+
+---
